702f57a222b7f816cd16969efe52eb11bbe48524
smasyutin <serg.masyutin@gmail.com>
2017-12-14 10:30:40 +0200

    - quick commit

--- src/main/java/myapps/JCCWorkshop.java
+++ src/main/java/myapps/JCCWorkshop.java
@@ -1,11 +1,12 @@
 package myapps;
 
-import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.Topology;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.*;
 import org.apache.kafka.streams.kstream.KStream;
 
 import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
 
 public class JCCWorkshop {
 
@@ -13,15 +14,38 @@ public class JCCWorkshop {
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-//        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, JsonSerde.class.getName());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, JsonSerde.class.getName());
 
+        JsonSerde<Login> loginSerde = new JsonSerde<>(Login.class);
         final StreamsBuilder builder = new StreamsBuilder();
-        KStream<String, String> loginSource = builder.stream("login-topic");
+        KStream<String, Login> loginSource = builder.stream(new Serdes.StringSerde(), loginSerde, "login-topic");
+//        KStream<String, String> clickSource = builder.stream("click-topic");
         loginSource.to("logout-topic");
 
         final Topology topology = builder.build();
 
         System.out.println(topology.describe());
+
+        final KafkaStreams streams = new KafkaStreams(topology, props);
+
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        // attach shutdown handler to catch control-c
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
+            @Override
+            public void run() {
+                streams.close();
+                latch.countDown();
+            }
+        });
+
+        try {
+            streams.start();
+            latch.await();
+        } catch (Throwable e) {
+            System.exit(1);
+        }
+        System.exit(0);
     }
 
 }
---
+++ src/main/java/myapps/Login.java
@@ -0,0 +1,23 @@
+package myapps;
+
+public class Login {
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    private String user;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    private String token;
+}
